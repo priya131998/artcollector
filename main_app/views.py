be929145061914755055ccbe2483f30a9784f8a3
priya131998 <priya.magiya@gmail.com>
2021-08-17 07:55:06 +0800

    add a create class

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 from .models import Art
 # Create your views here.
 
@@ -21,3 +22,6 @@ def arts_detail(request, art_id):
   art = Art.objects.get(id=art_id)
   return render(request, 'arts/detail.html', { 'art': art })
 
+class ArtCreate(CreateView):
+  model = Art
+  fields = '__all__'
