1c4dfea2c90464e11ebe3e76fb2dfff897d2d460
priya131998 <priya.magiya@gmail.com>
2021-08-18 05:19:39 +0800

    add the exhibition model

--- main_app/admin.py
+++ main_app/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Art
+from .models import Art, Exhibition
 
 # Register your models here
 admin.site.register(Art)
--- main_app/models.py
+++ main_app/models.py
@@ -4,6 +4,12 @@ from django.urls import reverse
 # Create your models here.
 # Add the Art class & list and view function below the imports
 
+SHOWS = (
+    ('S', 'Somnyama Ngonyama'),
+    ('W', 'WoA! Pt.2'),
+    ('P', 'Paradigm of Charcoal')
+)
+
 class Art(models.Model):
     name = models.CharField(max_length=100)
     title = models.CharField(max_length=100)
@@ -15,3 +21,20 @@ class Art(models.Model):
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'art_id': self.id})
+
+
+
+class Exhibition(models.Model):
+  date = models.DateField()
+  show = models.CharField(
+    max_length=1,
+	 choices=SHOWS,
+	 default=SHOWS[0][0]
+  )
+  # Create a cat_id FK
+  art = models.ForeignKey(Art, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_show_display()} on {self.date}"
+    
+    
