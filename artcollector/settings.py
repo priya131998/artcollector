f136df57d1ee6165a9bb1c6ddb7f10e6089e1562
priya131998 <priya.magiya@gmail.com>
2021-08-19 06:59:14 +0800

    add log in function

--- artcollector/settings.py
+++ artcollector/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Add this variable to specify where successful logins should redirect to
+LOGIN_REDIRECT_URL = '/arts/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
