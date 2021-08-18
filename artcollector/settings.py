8cf79fa7f4afb643e4b5a6aa17fcdc80f7522252
priya131998 <priya.magiya@gmail.com>
2021-08-19 07:10:42 +0800

    add logout function

--- artcollector/settings.py
+++ artcollector/settings.py
@@ -122,6 +122,7 @@ STATIC_URL = '/static/'
 
 # Add this variable to specify where successful logins should redirect to
 LOGIN_REDIRECT_URL = '/arts/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
